87542829673cf6619e03842c569b7a0f552c2b18
razerautis <52868175+razerautis@users.noreply.github.com>
2022-03-19 20:23:10 +0700

    Update create-user-v3.py

--- create-user-v3.py
+++ create-user-v3.py
@@ -1,10 +1,9 @@
 #Script V3
 #Auto Created User Login
-#Created by: Bintang Zaquine
 
 import os
-username = "bintang" #@param {type:"string"}
-password = "zaquine" #@param {type:"string"}
+username = "andovi" #@param {type:"string"}
+password = "juventus" #@param {type:"string"}
 os.system(f"useradd -m {username}")
 os.system(f"adduser {username} sudo")
 os.system(f"echo '{username}:{password}' | sudo chpasswd")
