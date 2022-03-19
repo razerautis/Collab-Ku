88006ed7329b01ad24e23eb298ee969c8dd6a212
razerautis <52868175+razerautis@users.noreply.github.com>
2022-03-19 20:24:03 +0700

    Update re-active-v3.sh

--- re-active-v3.sh
+++ re-active-v3.sh
@@ -1,6 +1,5 @@
 #Script V3
 #Auto Re-Active X-RDP On Google Colab
-#Created by: Bintang Zaquine
 
 nohup ./ngrok tcp --region ap 3389 &>/dev/null &
 sudo apt install lxde > /dev/null 2>&1
@@ -12,7 +11,7 @@ echo "========================"
 echo "    YOUR X-RDP INFO"
 echo "========================"
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
-echo "Username: bintang"
-echo "Password: zaquine"
+echo "Username: andovi"
+echo "Password: juventus"
 echo "========================"
 sleep 9876543210
