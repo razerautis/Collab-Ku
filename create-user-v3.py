34b405a902e55900e812949b46495903695d80fb
razerautis <52868175+razerautis@users.noreply.github.com>
2022-04-24 14:18:14 +0700

    Update create-user-v3.py

--- create-user-v3.py
+++ create-user-v3.py
@@ -4,7 +4,9 @@
 import os
 username = "andovi" #@param {type:"string"}
 password = "juventus" #@param {type:"string"}
+print("Creating User and Setting it up")
 os.system(f"useradd -m {username}")
 os.system(f"adduser {username} sudo")
 os.system(f"echo '{username}:{password}' | sudo chpasswd")
 os.system("sed -i 's/\/bin\/sh/\/bin\/bash/g' /etc/passwd")
+print("User Created and Configured")
